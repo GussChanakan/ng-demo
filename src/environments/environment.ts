ecf88c6473961f53e0b7774cca8f397cc8e31f66
GussChanakan <chanakan.guss@gmail.com>
2025-11-01 10:47:25 +0700

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    ProjectName: 'Audit Document',
+    PathServer: '',
+
+    // ---- Server ----
+    API_URL: `http://192.168.1.201:9000/api`,
+    Report_URL: `http://192.168.1.201:9000/`,
+
+    Maintenance: false
+};
